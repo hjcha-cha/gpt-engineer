3afd79016654e2f91401a52518b806f89b3a4514
Jinyoung Jang <jyjang@uengine.org>
2024-02-25 11:58:13 +0000

    update

--- src/main/java/com/example/fooddelivery/front/dto/OrderRequest.java
+++
@@ -1,8 +0,0 @@
-package com.example.fooddelivery.front.dto;
-
-import lombok.Data;
-
-@Data
-public class OrderRequest {
-    // Fields related to the order request, e.g., items, customer info, etc.
-}
--- src/main/java/com/example/fooddelivery/front/repository/OrderRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.fooddelivery.front.repository;
-
-import com.example.fooddelivery.front.entity.Order;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface OrderRepository extends JpaRepository<Order, Long> {
-}
